c201ae1090387925b2c17a24cce47eedc255ec22
Tamilselvan <etamilselvan10@gmail.com>
2025-03-22 21:46:27 +0530

    commit

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -50,6 +50,14 @@ class LoadStreamlitUI:
                 # usecase selection
                 self.user_controls['selected_usecase']=st.selectbox(label="Select Usecase",options=usecase_options)
 
+                if self.user_controls=='Chatbot with Tool':
+                    # API key input
+                    os.environ['TAVILY_API_KEY']=self.user_controls['TAVILY_API_KEY']=st.session_state['TAVILY_API_KEY']=st.text_input(label='Tavily API Key',type='password')
+                    
+                    # Validate API key
+                    if not self.user_controls['TAVILY_API_KEY']:
+                        st.warning("⚠️ Please enter your TAVILY_API_KEY key to proceed. Don't have? refer : https://app.tavily.com/home")
+
 
                 if 'state' not in st.session_state:
                     st.session_state.state=self.initialize_session()
